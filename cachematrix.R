ec2620a5ba87e941b629c012d6e6d4c70cec6ccd
Roman Mindlin <mindlin@inbox.ru>
2016-02-06 09:26:26 +0300

    Assignment commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##R function that is able to cache potentially time-consuming computation
 
-## Write a short comment describing this function
+##makeCacheMatrix function creates a special "matrix", which is really a list containing a function to
+
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the solve
+## 4.get the value of the solve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## The following function calculates the solve of the special "matrix" created with the above function. However, it first checks
+## to see if the solve has already been calculated. If so, it gets the solve from the cache and skips the computation. 
+## Otherwise, it calculates the solve of the matrix and sets the value of the solve in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
